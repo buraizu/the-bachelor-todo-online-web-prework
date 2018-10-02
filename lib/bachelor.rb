c31c3a7bd63f5249e4873bfe7350ab497d287268
Bryce Eadie <bryce.eadie@gmail.com>
2018-10-02 19:10:20 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -11,6 +11,16 @@ def get_first_name_of_season_winner(data, season)
     end
   end
 end
+
+def get_contestant_name(data, occupation)
+  data.each do |number, info|
+    info.each do |collection|
+      if collection["occupation"] == occupation
+        return collection["name"]
+      end
+    end
+  end
+end
   
 def count_contestants_by_hometown(data, hometown)
   hometown_counter = 0
